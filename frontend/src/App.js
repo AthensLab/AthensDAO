68891c78fe5769c0c414bb0646959fd5f076dca9
Hico <hicoleer@gmail.com>
2022-01-13 14:30:38 +0200

    Display account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,20 +11,21 @@ function App() {
 
   const [signer, setSigner] = useState(undefined);
   const [alpha, setAlpha] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
 
   useEffect(() => {
     
     const init = async () => {
-      const { signer, alpha } = await getBlockchain();
+      const { signer, alpha, account } = await getBlockchain();
       setSigner(signer);
       setAlpha(alpha);
+      setAccount(account);
     };
     init();
 
   }, []);
 
   //////////////functions/////////////
-
 
 
   /////////////////////
@@ -34,13 +35,21 @@ function App() {
   ) {
     return 'DAPP is Loading...';
   }
+  // console.log(account)
 
   return (
     <div className="App">
+
+      <div className='container'>
+        <div className='row'>Connected Account : {account}</div>
+        <div className='row'>  </div>
+      </div>
+      
       <header className="App-header">
         <h1>ALPHA Token</h1>
         <button onClick={ function eth() {window.ethereum.request({ method: 'eth_requestAccounts' }); console.log('MM connected')}} className="enableEthereumButton">Connect MetaMask</button>
       </header>
+
     </div>
   );
 }
--- frontend/src/ethereum.js
+++ frontend/src/ethereum.js
@@ -13,15 +13,19 @@ const getBlockchain = () =>
                 const provider = new ethers.providers.Web3Provider(window.ethereum);
                 const signer = provider.getSigner();
 
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                const account = accounts[0];
+                // console.log(account);
+
                 const alpha = new Contract(
                     '0x4Fe7D522AbE6AC9699401c1C9a1bFDD869b95cdb',
                     Alpha.abi,
                     signer
                 );
-                resolve({signer, alpha});
+                resolve({signer, alpha, account});
             }
 
-            resolve({signer: undefined, alpha: undefined })
+            resolve({signer: undefined, alpha: undefined, account: undefined })
         });
 
     });
